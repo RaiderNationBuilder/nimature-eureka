d6058f653300baa896c54832c1a41484106602d0
RaiderNationBuilder <71417297+RaiderNationBuilder@users.noreply.github.com>
2021-11-10 18:53:11 -0600

    trying to get heroku to work

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const router = require('express').Router();
-const animalRoutes = require('../apiRoutes/animalRoutes');
+// const animalRoutes = require('../apiRoutes/animalRoutes');
 
-router.use(require('../apiRoutes/zookeeperRoutes'));
-router.use(animalRoutes);
+// router.use(require('../apiRoutes/zookeeperRoutes'));
+// router.use(animalRoutes);
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../public/index.html'));
